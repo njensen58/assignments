bf3d3843dc765d679a622a65efc88797b0751420
Nate Jensen <nathan.jensen.58@gmail.com>
2018-02-01 13:45:39 -0700

    gallery-page

--- projects/business-time/app.js
+++ projects/business-time/app.js
@@ -1,7 +1,6 @@
 document.socialForm.addEventListener("submit", function(e){
     e.preventDefault();
     var firstName = document.socialForm.firstName.value;
-    var lastName = document.socialForm.firstName.value;
     var submittedDiv = document.getElementById('submitted');
     document.socialForm.style.display = "none";
     submittedDiv.textContent = "Submitted," + "\n Thanks " + firstName + "!";
