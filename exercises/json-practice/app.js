d7065e9784f39afb402a942aa446d94a3d9ccf35
Nate Jensen <nathan.jensen.58@gmail.com>
2018-02-10 11:44:22 -0700

    zen-garden-query601px

---
+++ exercises/json-practice/app.js
@@ -0,0 +1,8 @@
+// JSON object with 4 or more properties
+
+{
+    "name": "Nate",
+    "age": 28,
+    "hobbies": ["Drawing", "Coding", "Guitar"],
+    "isAlive": true
+}
---
+++ exercises/json-practice/app2.js
@@ -0,0 +1,20 @@
+// JSON array with 3 ore more objects
+
+[
+    "count", 3,
+    {
+        "name": "Rick",
+        "age": 72,
+        "profession": "Scientist"
+    },
+    {
+        "name": "Morty",
+        "age": 14,
+        "profession": "Student"
+    },
+    {
+        "name": "Mr. Poopie Butthole",
+        "age": "unknown",
+        "profession": "unknown"
+    }
+]
