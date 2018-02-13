ae688ce786071bcdfb941885f35266cab3f664c0
Nate Jensen <nathan.jensen.58@gmail.com>
2018-02-13 12:34:00 -0700

    axios-todo-complete

--- projects/axios-todo-practice/app.js
+++ projects/axios-todo-practice/app.js
@@ -6,8 +6,12 @@ const todoPrice = document.getElementById('userPrice');
 const todoDescribe = document.getElementById('userDescription');
 const todoImg = document.getElementById('userImage');
 const btn = document.getElementById('btn');
+const btn2 = document.getElementById('btn2');
+const idDel = document.getElementById('del');
 let imgTag = '';
+let id = '';
 
+// _id:
 
 
 
@@ -16,7 +20,7 @@ axios.get('https://api.vschool.io/nateJ/todo/').then(response => {
     imgTag = response.data[i].imgUrl ? `<img src="${response.data[i].imgUrl}" class="imgEdit"/>` : "";
 
     document.getElementById('list').innerHTML += `<input type="checkbox" class="checker">
-    <li class="strike">${imgTag}${response.data[i].title}<span class="deleteSpan"><i class="ion-android-delete delete"></span></li>`;
+    <li class="strike">${imgTag}${response.data[i].title}<br/><span class="idSpan">ID: ${response.data[i]._id}</span></li>`;
     }
 })
 
@@ -30,43 +34,26 @@ btn.addEventListener("click", function(e){
                 };
     imgTag = newTodo.imgUrl ? `<img src="${newTodo.imgUrl}" class="imgEdit"/>` : "";
     if(newTodo.title === " "){
-        alert("Title must not be empty")
+        alert("Your Todo must have a Title")
     } else {
         axios.post('https://api.vschool.io/nateJ/todo/', newTodo);
-        document.getElementById('list').innerHTML += `<input type="checkbox" class="checker"><li class="strike">${imgTag}${newTodo.title}<span class="deleteSpan"><i class="ion-android-delete delete"></span></li>`;
+        document.getElementById('list').innerHTML += `<input type="checkbox" class="checker"><li class="strike">${imgTag}${newTodo.title}</li>`;
     }
 
 })
 
+btn2.addEventListener("click", function() {
+    if(idDel.value === ''){
+        alert("Please copy the ID from the Todo you want to delete");
+    } else {
+    axios.delete(`https://api.vschool.io/nateJ/todo/${idDel.value}`).then(alert("Todo successfully deleted"));
+    }
+})
 
-const icons = document.getElementsByTagName('i');
 
-// for(var i = 0; i < icons.length; i++){
-//     icons[i].addEventListner("click",function(){
+
+// const icons = document.getElementsByTagName('i');
 //
-//     })
-// }
-// icons.addEventListener("click", function(){
-//
+// icons[1].addEventListener("click", function(){
+//     console.log('hellow')
 // })
-
-
-
-
-
-
-
-
-// var logo = document.getElementById('rm');
-// logo.setAttribute('src', 'img/rm2.png');
-
-
-
-
-
-
-
-
-
-// A user should be able to attach an imgUrl to the item (and if there is an imgUrl, the image should be displayed)
-// imgUrl: string;
