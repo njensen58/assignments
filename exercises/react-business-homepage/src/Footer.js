90498f22d068af7b6895eba8121cf558f38ffa26
Nate Jensen <nathan.jensen.58@gmail.com>
2018-02-15 09:48:05 -0700

    react-business-footerMid

--- exercises/react-business-homepage/src/Footer.js
+++ exercises/react-business-homepage/src/Footer.js
@@ -3,16 +3,24 @@ import React from 'react';
 function Footer(){
     return (
         <div className="footerDiv">
-            <form name="subscribeForm">
-                <formfield>
-                    <input type="text" placeholder="What do we call you?"/>
-                    <input type="text" placeholder="Email Adress"/>
-                    <label htmlFor="subscribeForm"><button type="Submit">Submit</button></label>
-                </formfield>
-            </form>
-            <p>
-                Copyright - 2018
-            </p>
+            <div className="footerLeftFill"></div>
+            <div className="formDiv">
+                <form name="subscribeForm">
+                    <formfield>
+                        <input type="text" placeholder="What do we call you?"/>
+                        <input type="email" placeholder="Email Adress"/>
+                        <label htmlFor="subscribeForm"><button type="Submit" className="submitBtn">Submit</button></label>
+                    </formfield>
+                </form>
+            </div>
+            <div className="footerRightFill"></div>
+            <div className="iconsDiv">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
         </div>
     )
 }
