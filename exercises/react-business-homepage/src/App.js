71718e30817fdb3d2e7438477a366306a1b3e945
Nate Jensen <nathan.jensen.58@gmail.com>
2018-02-16 08:56:16 -0700

    react-businessPage-complete

--- exercises/react-business-homepage/src/App.js
+++ exercises/react-business-homepage/src/App.js
@@ -11,8 +11,8 @@ import './style.css';
 
 function App(){
     return (
-        <div>
-            <div>
+        <div className="mobileContainer">
+            <div className="topContainer">
             <div className="pageContainer">
                 <Navbar />
                 <Header />
--- exercises/react-business-homepage/src/Footer.js
+++ exercises/react-business-homepage/src/Footer.js
@@ -7,8 +7,9 @@ function Footer(){
             <div className="formDiv">
                 <form name="subscribeForm">
                     <formfield>
-                        <input type="text" placeholder="What do we call you?"/>
-                        <input type="email" placeholder="Email Adress"/>
+                        <h3>Send me a Request!</h3>
+                        <input type="text" placeholder="What is your name?"/>
+                        <input type="email" placeholder="Email Address"/>
                         <label htmlFor="subscribeForm"><button type="Submit" className="submitBtn">Submit</button></label>
                     </formfield>
                 </form>
@@ -16,9 +17,9 @@ function Footer(){
             <div className="footerRightFill"></div>
             <div className="iconsDiv">
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li><i className="ion-social-googleplus icon1"></i></li>
+                    <li><i className="ion-social-github icon2"></i></li>
+                    <li><i className="ion-social-twitter icon3"></i></li>
                 </ul>
             </div>
         </div>
